a6b110f2429a640ed3857bdb17be725a067091a2
hfvaronb <hector.varon@gmail.com>
2025-03-06 22:04:14 -0500

    Ajuste archivos ej. 08

--- Basic-hfvaronb/08-conditionals.js
+++ Basic-hfvaronb/08-conditionals.js
@@ -2,26 +2,58 @@
 
 // if (si)
 
-let age = 18
+let age = 18;
 
 if (age == 37) {
+
     console.log("La edad es 37")
 }
 
-// else (si no)
+// else  (si no)
 
 if (age == 37) {
     console.log("La edad es 37")
-} else {
-    console.log("La edad no es 37")
-}
-
-// else if (si no, si)
-
-if (age == 37) {
-    console.log("La edad es 37")
-} else if(age < 18) {
+} else if (age < 18) {
     console.log("Es menor de edad")
-}else{
+} else {
     console.log("La edad no es 37 ni es menor de edad")
 }
+
+
+// operador ternario
+
+const message = age == 37 ? "La edad es 37 - ternario" : "Ternario La edad no es 37"
+console.log(message)
+
+// switch
+
+let dayName
+let day = 4
+
+switch (day) {
+    case 0:
+        dayName = "Lunes"
+        break
+    case 1:
+        dayName = "Martes"
+        break
+    case 2:
+        dayName = "Miercoles"
+        break
+    case 3:
+        dayName = "Jueves"
+        break
+    case 4:
+        dayName = "Viernes"
+        break
+    case 5:
+        dayName = "Sabado"
+        break
+    case 6:
+        dayName = "Domingo"
+        break
+    default:
+        dayName = "Número de día incorrecto"
+}
+
+console.log(dayName)
